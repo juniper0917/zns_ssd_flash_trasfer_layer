4b9486264cdef6cdd6b17e288f68e4261e209a93
seunghunYu <juniper0917@gmail.com>
2023-03-30 06:53:13 +0000

    cmake, library install

---
+++ msc-stosys-sramework/CMakeFiles/m1.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/user/zns_ssd_flash_trasfer_layer/msc-stosys-sramework/src/common/nvmeprint.cpp" "/home/user/zns_ssd_flash_trasfer_layer/msc-stosys-sramework/CMakeFiles/m1.dir/src/common/nvmeprint.cpp.o"
+  "/home/user/zns_ssd_flash_trasfer_layer/msc-stosys-sramework/src/common/utils.cpp" "/home/user/zns_ssd_flash_trasfer_layer/msc-stosys-sramework/CMakeFiles/m1.dir/src/common/utils.cpp.o"
+  "/home/user/zns_ssd_flash_trasfer_layer/msc-stosys-sramework/src/m1/device.cpp" "/home/user/zns_ssd_flash_trasfer_layer/msc-stosys-sramework/CMakeFiles/m1.dir/src/m1/device.cpp.o"
+  "/home/user/zns_ssd_flash_trasfer_layer/msc-stosys-sramework/src/m1/m1.cpp" "/home/user/zns_ssd_flash_trasfer_layer/msc-stosys-sramework/CMakeFiles/m1.dir/src/m1/m1.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
